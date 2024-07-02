8fca0df732e18f338028d375d3f31eb70455b3b3
ethan <ethan@pop-os.lan>
2024-07-02 14:05:03 -0700

    made view for getting user profile data. Made admin account for testing, username:admin, password:password

--- bounty_hunter/user_profiles/admin.py
+++ bounty_hunter/user_profiles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserAccount,UserProfileInfo
 
 # Register your models here.
+
+admin.site.register(UserAccount)
+admin.site.register(UserProfileInfo)
--- bounty_hunter/user_profiles/views.py
+++ bounty_hunter/user_profiles/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+#temporary
+from django.template import loader
+
+from .models import UserProfileInfo, LinkedAccounts, UserAccount
+
+def profile(request, request_username):
+    request_owner = UserAccount.objects.get(username_text=request_username)
+    user_profile = UserProfileInfo.objects.get(owner=request_owner)
+    bio = user_profile.bio_text
+    pfp = user_profile.profile_image
+    name = user_profile.owner.username_text
+    context = {"bio" : bio,
+               "pfp" : pfp,
+               "name" : name}
+    
+    #temporary interface
+    template = loader.get_template("user_profiles/profile.html")
+    
+    return HttpResponse(template.render(context,request))
+
+
 
 # Create your views here.
