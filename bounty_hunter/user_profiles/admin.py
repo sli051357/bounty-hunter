095329ced1cb4227b46576a0146eab79d7bf3f97
ethan <ethan@pop-os.lan>
2024-07-02 14:33:06 -0700

    added profile link displaying in backend  for profile view.

--- bounty_hunter/user_profiles/admin.py
+++ bounty_hunter/user_profiles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import UserAccount,UserProfileInfo
+from .models import UserAccount,UserProfileInfo,LinkedAccounts
 
 # Register your models here.
 
 admin.site.register(UserAccount)
+admin.site.register(LinkedAccounts)
 admin.site.register(UserProfileInfo)
--- bounty_hunter/user_profiles/views.py
+++ bounty_hunter/user_profiles/views.py
@@ -9,17 +9,22 @@ from .models import UserProfileInfo, LinkedAccounts, UserAccount
 def profile(request, request_username):
     request_owner = UserAccount.objects.get(username_text=request_username)
     user_profile = UserProfileInfo.objects.get(owner=request_owner)
-    bio = user_profile.bio_text
-    pfp = user_profile.profile_image
-    name = user_profile.owner.username_text
-    context = {"bio" : bio,
-               "pfp" : pfp,
-               "name" : name}
-    
-    #temporary interface
+    linked_accs = LinkedAccounts.objects.filter(owner=request_owner)
+
+    headers = {
+        "bio":user_profile.bio_text,
+        "pfp":user_profile.profile_image,
+        "name":request_owner.username_text
+    }
+
+    #adds account links to HttpResponse Dictionary
+    for i in range(0,len(linked_accs)):
+        headers["acc_link_"+str(i)] = linked_accs[i].account_text
+
+    #temporary html template render
     template = loader.get_template("user_profiles/profile.html")
-    
-    return HttpResponse(template.render(context,request))
+
+    return HttpResponse(template.render(headers,request))
 
 
 
