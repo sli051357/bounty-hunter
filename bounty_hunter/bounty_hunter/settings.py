4612974901fc10bb3b14a7bb229201fb18168f58
ethan <ethan@pop-os.lan>
2024-07-02 10:58:59 -0700

    enabled user_profiles

--- bounty_hunter/bounty_hunter/settings.py
+++ bounty_hunter/bounty_hunter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user_profiles.apps.UserProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
