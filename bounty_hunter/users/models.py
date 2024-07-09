c517a9a3666ca90bfd63b7bd63eb64e9d25d0f4c
ethan <ethan@pop-os.lan>
2024-07-09 10:46:29 -0700

    added edit views

--- bounty_hunter/users/models.py
+++ bounty_hunter/users/models.py
@@ -14,6 +14,7 @@ class UserProfileInfo(models.Model):
 
 #model for storing linked accounts. Many linked accounts may share a single owner.
 class LinkedAccounts(models.Model):
+    id = models.AutoField(primary_key=True)
     account_text = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
