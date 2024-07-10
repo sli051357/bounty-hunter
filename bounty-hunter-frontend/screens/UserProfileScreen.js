98251ed790c44dcef4cc14ee45509a16da5349c4
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2024-07-09 20:34:29 -0700

    Started UI of user profile and layout

--- bounty-hunter-frontend/screens/UserProfileScreen.js
+++ bounty-hunter-frontend/screens/UserProfileScreen.js
@@ -1,12 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { GLOBAL_STYLES } from "../constants/styles";
+
+
 
 function UserProfileScreen(){
+    const navigation = useNavigation();
+    console.log(navigation);
+
     return (
-        <View>
-            <Text>This is the user profile screen.</Text>
+        <View style={styles.page}>
+            <View>
+                <View>
+                    <Image style={styles.profilePicture} source={require('../assets/profile.jpeg')}/>
+                    <View>
+                        <Text>USERNAME</Text>
+                        <Text>ID</Text>
+                    </View>
+                </View>
+                <View>
+                    <Text>Rating and Friends</Text>
+                </View>
+            </View>
+            <View>
+                <Text>Will make component for resuability of About Me, Payment Methods, Recent Favors</Text>
+                <TextInput />
+            </View>
         </View>
     )
 
 }
 
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: GLOBAL_STYLES.colors.brown300,
+        flex: 1
+    },
+    profilePicture: {
+
+    }
+})
 export default UserProfileScreen;
