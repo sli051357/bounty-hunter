11f3a3f834aaa01e08f0651fbed03191df50cc3e
Ethan Han <e7han@ucsd.edu>
2024-07-29 21:24:37 -0700

    added friend request system and friend slist

--- bounty_hunter/users/models.py
+++ bounty_hunter/users/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 import PIL
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 # Create your models here.
 
 #model for storing bio and profile image. Each user has only one owner.
 class UserProfileInfo(models.Model):
+    friends = models.ManyToManyField("User",blank=True)
     bio_text = models.CharField(max_length=200)
     profile_image = models.ImageField(upload_to='res/')
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.owner.username
-    
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey("User", on_delete=models.CASCADE)    
+    to_user = models.ForeignKey("User", on_delete=models.CASCADE)    
+
 
 #model for storing linked accounts. Many linked accounts may share a single owner.
 class LinkedAccounts(models.Model):
