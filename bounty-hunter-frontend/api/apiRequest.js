998f6c9aeea6c573947de88043c88f6ef4af829c
ejnguyen1 <ejnguyen@ucsd.edu>
2024-07-31 16:01:20 -0500

    Completed documentation

--- bounty-hunter-frontend/api/apiRequest.js
+++ bounty-hunter-frontend/api/apiRequest.js
@@ -24,7 +24,7 @@ const apiService = {
     }, 
 
     // username = "username"
-    // data = {"accounts":linked_accs}
+    // returns = {"accounts":linked_accs}
     getUserLinks: async (username) => {
         try {
             const response = await axiosInstance.get(`/users/profiles/${username}/links`);
