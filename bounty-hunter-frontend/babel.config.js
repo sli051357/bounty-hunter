50e4e435ad01bb0af54d9b3e24c1b2965dbc3371
Ethan Han <e7han@ucsd.edu>
2024-07-31 00:05:17 -0700

    gave up on eslint

--- bounty-hunter-frontend/babel.config.js
+++ bounty-hunter-frontend/babel.config.js
@@ -1,20 +1,9 @@
-{
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-native/all"
-  ],
-  "plugins": ["react", "react-native"],
-  "env": {
-    "browser": true,
-    "node": true
-  },
-  "parserOptions": {
-    "ecmaVersion": 2020,
-    "sourceType": "module"
-  },
-  "rules": {
-    "react/prop-types": "off", // Example rule configuration
-    "react-native/no-inline-styles": "off"
-  }
-}
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
--- bounty-hunter-frontend/eslint.config.mjs
+++ bounty-hunter-frontend/eslint.config.mjs
@@ -1,11 +1,39 @@
+import react from "eslint-plugin-react";
+import reactNative from "eslint-plugin-react-native";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fixupPluginRules } from "@eslint/compat";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+export default [...compat.extends(
+    "eslint:recommended",
+), {
+    plugins: {
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+
+    rules: {
+        "react/prop-types": "off",
+        "react-native/no-inline-styles": "off",
+        "react/react-in-jsx-scope": "off",
+    },
+}];
