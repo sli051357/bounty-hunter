a0c88b101ca48fa51e3c4dff24cd56e263686771
Hyeonseo Jang <heather@Heather.lan>
2024-08-01 22:10:07 -0700

    debugg

--- bounty_hunter/wishlist/views.py
+++ bounty_hunter/wishlist/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Wishlist
+from .forms import WishlistForm
 
-# Create your views here.
+class WishlistView(View):
+    def get(self, request):
+        items = Wishlist.objects.all()
+        return render(request, 'wishlist/wishlist.html', {'items': items})
+
+class AddWishlistItemView(View):
+    def get(self, request):
+        form = WishlistForm()
+        return render(request, 'wishlist/add_item.html', {'form': form})
+    
+    def post(self, request):
+        form = WishlistForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('wishlist')
+        return render(request, 'wishlist/add_item.html', {'form': form})
+
+class RemoveWishlistItemView(View):
+    def post(self, request, pk):
+        item = Wishlist.objects.get(pk=pk)
+        item.delete()
+        return redirect('wishlist')
