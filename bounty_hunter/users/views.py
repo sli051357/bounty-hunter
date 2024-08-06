3109f5537d6ad456214d18983408865bdffab307
ejnguyen1 <ejnguyen@ucsd.edu>
2024-08-05 17:19:28 -0700

    debugged from testing

--- bounty_hunter/users/views.py
+++ bounty_hunter/users/views.py
@@ -140,11 +140,11 @@ def reject_friend_request(request, pk):
 # @login_required
 def remove_friend(request, request_username):
     curr_user = UserProfileInfo.objects.get(owner=request.user)
-    friend = User.objects.filter(username=request_username)
+    friend = User.objects.get(username=request_username)
     # check if user is a friend of curr_user
-    if curr_user.friends.filter(username=request_username).exists():
+    if User.objects.get(username=request_username) in curr_user.friends.all():
         curr_user.friends.remove(friend)
-        if not curr_user.friends.filter(username=request_username).exists():   # successfully removed
+        if User.objects.get(username=request_username) not in curr_user.friends.all():   # successfully removed
             return JsonResponse({"success":True})
         else:
             return JsonResponse({"success":False})
