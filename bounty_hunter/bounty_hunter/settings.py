338475a1f6d40b6fd88b6b147989bc2271300b8f
ejnguyen1 <ejnguyen@ucsd.edu>
2024-08-19 10:52:30 -0700

    added static files in settings and edited nginx config on ubuntu server

--- bounty_hunter/bounty_hunter/settings.py
+++ bounty_hunter/bounty_hunter/settings.py
@@ -142,6 +142,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
