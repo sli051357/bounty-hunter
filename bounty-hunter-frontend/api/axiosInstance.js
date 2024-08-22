1fb5d07ada5471acafc33301f546ebbcb7351483
Ethan Han <e7han@ucsd.edu>
2024-08-22 14:21:13 -0700

    changed url and fixed refresh in previous commit

--- bounty-hunter-frontend/api/axiosInstance.js
+++ bounty-hunter-frontend/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	//baseURL: "http://132.249.238.228", // Replace with your API's base URL
-	baseURL: "http://10.0.2.2:8000",
+	baseURL: "http://132.249.238.228", // Replace with your API's base URL
+	//baseURL: "http://10.0.2.2:8000",
 	headers: { "Content-Type": "application/json" },
 	proxy: false,
 });
