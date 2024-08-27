ade6bd4a6bb71ed27b154f403d05c69bce540db8
ejnguyen1 <ejnguyen@ucsd.edu>
2024-08-27 14:38:34 -0700

    in remove_friend method added code to remove current user from friend's friends list

--- bounty_hunter/users/views.py
+++ bounty_hunter/users/views.py
@@ -317,14 +317,20 @@ def reject_friend_request(request, pk):
 def remove_friend(request, request_username):
     curr_user = UserProfileInfo.objects.get(owner=request.user)
     friend = User.objects.get(username=request_username)
+    friend_user = UserProfileInfo.objects.get(owner=friend)
     # check if user is a friend of curr_user
     if User.objects.get(username=request_username) in curr_user.friends.all():
+        # remove friend from current user's friends list
         curr_user.friends.remove(friend)
+        # remove current user from friend's friends list
+        friend_user.friends.remove(request.user)
 
     elif User.objects.get(username=request_username) in curr_user.favoritedFriends.all():
         curr_user.favoritedFriends.remove(friend)
+        # remove current user from friend's friends list
+        friend_user.favoritedFriends.remove(request.user)
     
-    if User.objects.get(username=request_username) not in curr_user.friends.all() and User.objects.get(username=request_username) not in curr_user.favoritedFriends.all():   # successfully removed
+    if User.objects.get(username=request_username) not in curr_user.friends.all() and User.objects.get(username=request_username) not in curr_user.favoritedFriends.all() and request.user not in friend_user.friends.all() and request.user not in friend_user.favoritedFriends.all():   # successfully removed
         return JsonResponse({"status":"success"})
     else:
         return JsonResponse({"status":"fail"})
